811a92933c03fe5d925ea9cbf6efd3e3d527092e
Jonas Äikäs <jonas.aikas@hotmail.com>
2019-09-19 14:46:08 +0200

    Övning1

---
+++ Övning1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercises_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange två tal!"); 
+
+            Console.Write("Tal 1:"); 
+            int tal1=Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("Tal 2:"); 
+            int tal2=Convert.ToInt32(Console.ReadLine()); 
+
+            int sum=tal1+tal2; 
+
+            Console.WriteLine("Summan av dina tal blir: "+sum); 
+        }
+    }
+}
