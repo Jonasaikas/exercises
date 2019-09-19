ba0b9a60ea8427f3e2548e48a049a172fc87a3b1
Jonas Äikäs <jonas.aikas@hotmail.com>
2019-09-19 15:15:47 +0200

    Övning2

---
+++ Övning2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Övning2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Användaren skriver in decimaltal
+           Console.Write("Skriv in ett decimaltal:");
+           double tal=Convert.ToDouble(Console.ReadLine());
+            
+            //Convertar till heltal
+           int tal1=Convert.ToInt16(tal+0.01);
+
+           //Skriver ut heltalet
+           Console.WriteLine("Ditt tal avrunat till närmsta heltal är: "+tal1);
+
+        }
+    }
+}
